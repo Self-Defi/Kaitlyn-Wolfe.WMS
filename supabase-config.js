41222a15d1ba174111892b2785ddb54564755b7b
Self Defi <cbp.cep@gmail.com>
2026-03-15 21:14:45 -0700

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // Supabase connection config
 
 const SUPABASE_URL = "https://yscuhozzbjlzfqgxxlgr.supabase.co";
-const SUPABASE_KEY = "sb_publishable_j0e4kjoU4YYL03sAucnhYQ_yfzDAetp";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzY3Vob3p6YmpsemZxZ3h4bGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MTkzNDUsImV4cCI6MjA4OTE5NTM0NX0.10CmyFD5P_OswFNfcpv7JLsIX-xAwmYiLdqmTjE5me8";
 
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
